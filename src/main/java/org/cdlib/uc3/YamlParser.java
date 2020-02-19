4ee94a13d11dd78c8ceae1f94af1950a4cec5651
Terry Brady <terrywbrady@gmail.com>
2020-02-18 16:43:59 -0800

    Simplify with snakeyml and gson

--- src/main/java/org/cdlib/uc3/YamlParser.java
+++ src/main/java/org/cdlib/uc3/YamlParser.java
@@ -1,22 +1,34 @@
 package org.cdlib.uc3;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.yaml.snakeyaml.Yaml;
+
+/*
+ * The following example might be useful if it is necessary to preserve the full text of an anchor reference
+ * https://github.com/Xceptance/xlt-nocoding/commit/75f2f78542e529b56cf4f763073475f5f25c60e1
+ */
 
 public class YamlParser {
     
-    
-    public static void main(String[] argv) {
-    	ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+     public static void main(String[] argv) {
+    	String fs = (argv.length == 0) ? "config.yml" : argv[0];
+    	File f = new File(fs);
+        final Yaml yaml = new Yaml();
+
     	try {
-			System.out.println(mapper.readTree(new File("config.yml")));
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+            final Object loadedYaml = yaml.load(new FileReader(f));
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(loadedYaml,LinkedHashMap.class);
+
+            System.out.println(json);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
