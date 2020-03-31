e9a77e402bc3a2779e98ba0ad14864e0e474ea43
Terry Brady <terrywbrady@gmail.com>
2020-03-30 17:27:51 -0700

    add aws calls

--- README.md
+++ README.md
@@ -7,4 +7,10 @@
 `java -jar target/YamlParser-1.0.jar [yaml file]`
 
 ## Note
-`test.yml` has a sample file with an anchor and reference example. 
+`test.yml` has a sample file with an anchor and reference example.
+
+## AWS EC2 Setup
+```
+sudo yum install java-1.8.0-openjdk
+sudo yum install maven
+``` 
--- src/main/java/org/cdlib/uc3/YamlParser.java
+++ src/main/java/org/cdlib/uc3/YamlParser.java
@@ -1,28 +1,113 @@
 package org.cdlib.uc3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import org.yaml.snakeyaml.Yaml;
 
 public class YamlParser {
+    final Yaml yaml = new Yaml();
+    private LinkedHashMap<String, Object> loadedYaml = new LinkedHashMap<>();
+    private LinkedHashMap<String, Object> resolvedYaml = new LinkedHashMap<>();
+    private String ssmPrefix = "";
+    private AWSSimpleSystemsManagement ssm = AWSSimpleSystemsManagementClientBuilder.defaultClient();
+
     
-     public static void main(String[] argv) {
-    	String fs = (argv.length == 0) ? "config.yml" : argv[0];
+	public YamlParser(String ssmPrefix) {
+		this.ssmPrefix = ssmPrefix;
+	}
+	
+	public void parse(String fs) throws FileNotFoundException {
     	File f = new File(fs);
-        final Yaml yaml = new Yaml();
+    	loadedYaml = yaml.load(new FileReader(f));
+	}
+	
+	public String dumpJson() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(resolvedYaml, resolvedYaml.getClass());		
+	}
+	
+	public void resolveValues() {
+		resolvedYaml = resolveValues(loadedYaml);
+	}
 
+	public LinkedHashMap<String, Object> resolveValues(LinkedHashMap<String, Object> lmap) {
+		LinkedHashMap<String, Object> copy = new LinkedHashMap<>();
+		for(String k: lmap.keySet()) {
+			Object obj = lmap.get(k);
+			copy.put(k, resolveObjectValue(obj));
+		}
+		return copy;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Object resolveObjectValue(Object obj) {
+		if (obj instanceof LinkedHashMap) {
+			return resolveValues((LinkedHashMap<String, Object>)obj);
+		} else if (obj instanceof ArrayList) {
+			ArrayList<Object> copy = new ArrayList<>();
+			for(Object aobj: (ArrayList<Object>)obj) {
+				copy.add(resolveObjectValue(aobj));
+			}
+			return copy;
+		} else if (obj instanceof String) {
+			return resolveObjectValue((String)obj);
+		} else {
+			return obj;				
+		}
+	}
+
+	public static Pattern pToken = Pattern.compile("\\{!(ENV|SSM):\\s*([^\\}!]*)(!DEFAULT:\\s([^\\}]*))?\\}");
+	
+	public String getValue(String a, String def) {
+		return a == null ? def : a;
+	}
+	
+	public String resolveObjectValue(String s) {
+		Matcher m = pToken.matcher(s);
+		if (m.matches()) {
+			String type = getValue(m.group(1), "");
+			String key = getValue(m.group(2), "");
+			String def = getValue(m.group(4), String.format("*%s-%s*", type, key));
+			
+			if (type.equals("ENV")) {
+				return getValue(System.getenv(m.group(2)), def);
+			}
+			if (type.equals("SSM")) {
+			    GetParameterRequest request = new GetParameterRequest();
+			    request.setName(this.ssmPrefix + key);
+			    try {
+					String value = this.ssm.getParameter(request).getParameter().getValue();
+					return getValue(value, def);
+			    } catch(Exception e) {
+			    	return def;
+			    }
+			}
+			
+			return def;
+		}
+		return s;
+	}
+	
+    public static void main(String[] argv) {
+    	String fs = (argv.length == 0) ? "config.yml" : argv[0];
+    	YamlParser yp = new YamlParser("/demo/service1/dev/");
     	try {
-            final Object loadedYaml = yaml.load(new FileReader(f));
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String json = gson.toJson(loadedYaml,LinkedHashMap.class);
-
-            System.out.println(json);
+        	yp.parse(fs);
+        	yp.resolveValues();
+        	System.out.println(yp.dumpJson());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
